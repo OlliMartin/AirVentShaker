09a30b536dd2ff7af604a8e77f149d82ddb010bc
Olli Martin <scholl3ss93@gmail.com>
2024-12-22 15:30:34 +0100

    Comments adjusted

--- Oma.WndwCtrl.Core/Model/Sensor.cs
+++ Oma.WndwCtrl.Core/Model/Sensor.cs
@@ -4,6 +4,7 @@ namespace Oma.WndwCtrl.Core.Model;
 
 /// <summary>
 /// A read-only control that provides one or multiple data points
+/// Can define a GET endpoint to query the _current_ state (ad-hoc execution)
 /// </summary>
 public class Sensor : IStateQueryable
 {
--- Oma.WndwCtrl.Core/Model/Switch.cs
+++ Oma.WndwCtrl.Core/Model/Switch.cs
@@ -3,7 +3,9 @@ using Oma.WndwCtrl.Core.Interfaces;
 namespace Oma.WndwCtrl.Core.Model;
 
 /// <summary>
-/// A read+write control indicating on/off. 
+/// A read+write control indicating on/off.
+/// Can define a GET endpoint to query the _current_ state (ad-hoc execution)
+/// Additionally, a POST:/on and POST:/off endpoint is hosted (potentially flip as well)
 /// </summary>
 public class Switch : IStateQueryable
 {
