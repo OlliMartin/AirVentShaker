18cd2fce2ccb19f003b5b6d0f4187d0b57313f3c
OlliMartin <scholl3ss93@gmail.com>
2025-01-30 07:35:04 +0100

    Add mock project for os-agnostic CLI invocations

---
+++ .unitTests/Oma.WndwCtrl.Core.Mocks/CliCommandMock.cs
@@ -0,0 +1,108 @@
+﻿using System.Text.RegularExpressions;
+using CommandLine;
+using CommandLine.Text;
+using JetBrains.Annotations;
+
+namespace Oma.WndwCtrl.Core.Mocks;
+
+public static partial class StringExtensions
+{
+  [GeneratedRegex(@"\r\n|\n|\\r\\n|\\n")]
+  private static partial Regex NewLineRegex();
+
+  public static IEnumerable<string> SplitToLines(this string input)
+  {
+    Regex split = NewLineRegex();
+    string[] res = split.Split(input);
+    return res;
+  }
+}
+
+[PublicAPI]
+public class CliCommandMock
+{
+  public static int Main(string[] args)
+  {
+    using Parser parser = new(
+      with =>
+      {
+        with.CaseSensitive = false;
+        with.CaseInsensitiveEnumValues = true;
+        with.EnableDashDash = true;
+      }
+    );
+
+    ParserResult<CliInvocationOptions>? parserResult =
+      parser.ParseArguments<CliInvocationOptions>(args);
+
+    if (parserResult.Tag != ParserResultType.NotParsed)
+    {
+      return RunOptions(parserResult.Value);
+    }
+
+    string helpText = HelpText.AutoBuild(parserResult).ToString();
+    Console.Error.WriteLine("Incorrect arguments provided. Please refer to the following help text:");
+    Console.WriteLine(helpText);
+
+    return 255; // Reserved exit code to indicate something is wrong with the invocation.
+  }
+
+  private static int RunOptions(CliInvocationOptions opts)
+  {
+    if (opts.ExitCode >= 255)
+    {
+      Console.Error.WriteLine("Exit code must be less than 255. This is not a valid test.");
+      return 255;
+    }
+
+    List<Action<string>> writers = [];
+
+    if (opts.TargetStreams.HasFlag(CliInvocationOptions.OutputTarget.StdOut))
+    {
+      writers.Add(Console.WriteLine);
+    }
+
+    if (opts.TargetStreams.HasFlag(CliInvocationOptions.OutputTarget.StdErr))
+    {
+      writers.Add(Console.Error.WriteLine);
+    }
+
+    WriteText(opts.Text, writers);
+
+    return opts.ExitCode;
+  }
+
+  private static void WriteText(string text, List<Action<string>> writers)
+  {
+    foreach (string line in text.SplitToLines())
+    foreach (Action<string> writer in writers)
+      writer(line);
+  }
+}
+
+[PublicAPI]
+public record CliInvocationOptions
+{
+  [Flags]
+  public enum OutputTarget
+  {
+    StdOut = 1,
+    StdErr = 2,
+  }
+
+  [Option(shortName: 'c', "code", Required = false, HelpText = "The exit code to return.")]
+  public int ExitCode { get; init; }
+
+  [Option(
+    shortName: 's',
+    "targetStreams",
+    Required = false,
+    HelpText = "If true, writes provided text to standard error instead of out."
+  )]
+  public OutputTarget TargetStreams { get; init; } = OutputTarget.StdOut;
+
+  [Option(shortName: 't', "text", Required = true, HelpText = "The text to write to standard out or error.")]
+  public string Text { get; init; } = string.Empty;
+
+  public override string ToString() => $"--code {ExitCode} --targetStreams {TargetStreams} --text \"{Text}\"";
+}
--- Oma.WndwCtrl.Core/Executors/Commands/CliCommandExecutor.cs
+++ Oma.WndwCtrl.Core/Executors/Commands/CliCommandExecutor.cs
@@ -1,11 +1,11 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using CliWrap;
 using JetBrains.Annotations;
 using LanguageExt;
 using Oma.WndwCtrl.Abstractions;
 using Oma.WndwCtrl.Abstractions.Errors;
 using Oma.WndwCtrl.Abstractions.Model;
+using Oma.WndwCtrl.Core.Errors.Commands;
 using Oma.WndwCtrl.Core.Model.Commands;
 using static LanguageExt.Prelude;
 
@@ -27,45 +27,40 @@ public class CliCommandExecutor : ICommandExecutor<CliCommand>
   {
     try
     {
-      StringBuilder stdOutBuffer = new();
-      StringBuilder stdErrBuffer = new();
-
-      Command cliBuilder = Cli.Wrap(command.FileName)
-        .WithArguments(command.Arguments ?? string.Empty)
-        .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
-        .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
-        .WithValidation(CommandResultValidation.None);
-
-      if (command.WorkingDirectory is not null)
+      ProcessStartInfo processStartInfo = new()
       {
-        cliBuilder = cliBuilder.WithWorkingDirectory(command.WorkingDirectory);
-      }
-
-      CommandResult result = await cliBuilder.ExecuteAsync(CancellationToken.None, cancelToken);
-
-      string stdOutRes = stdOutBuffer.ToString();
-      string toUse;
-
-      if (result.ExitCode == 0)
-      {
-        toUse = stdOutRes;
-      }
-      else
-      {
-        string stdErrRes = stdErrBuffer.ToString();
-
-        toUse = string.IsNullOrEmpty(stdErrRes)
-          ? stdOutRes
-          : stdErrRes;
-      }
-
-      CommandOutcome outcome = new(toUse)
-      {
-        Success = result.ExitCode == 0,
+        FileName = command.FileName,
+        Arguments = command.Arguments,
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
       };
 
+      using Process? process = Process.Start(processStartInfo);
+
+      if (process is null)
+      {
+        return Left<FlowError>(
+          new CliCommandError("Could not obtain process instance.", isExceptional: true, isExpected: false)
+        );
+      }
+
+      string errorText = string.Empty;
+      process.ErrorDataReceived += (sender, e) => { errorText += e.Data; };
+
+      process.BeginErrorReadLine();
+      string allText = await process.StandardOutput.ReadToEndAsync(cancelToken);
+
+      await process.WaitForExitAsync(cancelToken);
+
+      string outcome = process.ExitCode == 0 ? allText
+        : string.IsNullOrEmpty(errorText) ? allText : errorText;
+
       return Right(
-        outcome
+        new CommandOutcome(outcome)
+        {
+          Success = process.ExitCode == 0,
+        }
       );
     }
     catch (OperationCanceledException ex)
