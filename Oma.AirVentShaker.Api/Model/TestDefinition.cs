ea508741ef63abea51e4fb031069495da354fb3c
Olli Martin <scholl3ss93@gmail.com>
2026-04-06 16:44:01 +0200

    Update default test steps

--- Oma.AirVentShaker.Api/Model/TestDefinition.cs
+++ Oma.AirVentShaker.Api/Model/TestDefinition.cs
@@ -20,36 +20,19 @@ public class TestDefinition
       .AddTestStep(
         new()
         {
-          Frequency = 40,
+          Frequency = 10,
           Duration = TimeSpan.FromSeconds(15),
-          TargetGravitationalForce = 0.3f,
+          TargetGravitationalForce = 0.5f,
           Order = 1,
         }
       )
       .AddTestStep(
         new()
         {
-          Frequency = 50,
+          Frequency = 20,
           Duration = TimeSpan.FromSeconds(15),
           TargetGravitationalForce = 0.5f,
           Order = 2,
-        }
-      )
-      .AddTestStep(
-        new()
-        {
-          Frequency = 40,
-          Duration = TimeSpan.FromSeconds(30),
-          TargetGravitationalForce = 0.4f,
-          Order = 3,
-        }
-      ).AddTestStep(
-        new()
-        {
-          Frequency = 30,
-          Duration = TimeSpan.FromSeconds(45),
-          TargetGravitationalForce = 0.25f,
-          Order = 4,
         }
       );
   }
